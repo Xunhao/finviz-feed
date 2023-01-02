9712caba98abcb81ba9e040c87506921f848f301
Shin How Liu <shinhow02@gmail.com>
2023-01-02 12:57:12 +0800

    Modified try/except error handling to handle HTTP errors and added a successful response when script is completed

--- finviz-feed.py
+++ finviz-feed.py
@@ -5,12 +5,14 @@ import csv
 import os
 
 # Tickers
-tickers = ['AAPL', 'COST']
+tickers = ['AAPL', 'GOOG']
 
 
 def scraper(tickers):
     # Create a csv file to hold the scrapped data for all tickers
     current_directory = os.getcwd()
+    ticker_success_count = 0
+
     with open(f'{current_directory}/finviz_feed.csv', 'w') as finviz_feed:
         finviz_feed_header = ['Date Time', 'Ticker',
                               'News Title', 'News Link', 'News Source']
@@ -20,10 +22,14 @@ def scraper(tickers):
 
         for ticker in tickers:
             try:
-                finviz = requests.get(
-                    f'https://finviz.com/quote.ashx?t={ticker}', headers={'User-Agent': 'Mozilla/5.0'})
-            except Exceptions as e:
-                print(e)
+                finviz = requests.get(f'https://finviz.com/quote.ashx?t={ticker}', headers={'User-Agent': 'Mozilla/5.0'})
+                finviz.raise_for_status()  # Check for HTTP errors
+
+            except requests.exceptions.HTTPError as err:
+                if finviz.status_code == 404:  # URL not found which indicate that the ticker symbol cannot be found
+                    print(f'{finviz.status_code} Error Code. URL not found for ticker symbol {ticker}. Skipping...')
+                else:  # Handle any other HTTP errors
+                    print(err)
             else:
                 # To be initialised for each ticker
                 soup = BeautifulSoup(finviz.content, 'html.parser')
@@ -36,10 +42,8 @@ def scraper(tickers):
                     # The first news link of the day will always contain both date and time. Subsequent links will not have until it reaches the previous day
                     # Prepare the tags to scrap
                     news_section = news_table.find_all('tr')[i]
-                    news_div_left = news_section.find(
-                        'div', attrs={'class': 'news-link-left'})
-                    news_div_right = news_section.find(
-                        'div', attrs={'class': 'news-link-right'})
+                    news_div_left = news_section.find('div', attrs={'class': 'news-link-left'})
+                    news_div_right = news_section.find('div', attrs={'class': 'news-link-right'})
 
                     # Key data for each news link
                     news_td = news_section.find('td').string
@@ -57,20 +61,19 @@ def scraper(tickers):
                         break  # Move to the next ticker
                     else:
                         news_title = news_div_left.string  # Assign news title
-                        news_link = news_div_left.find(
-                            'a')['href']  # Assign news link
+                        news_link = news_div_left.find('a')['href']  # Assign news link
                         news_source = news_div_right.string.strip()  # Assign news source
 
-                        finviz_feed_data.append(
-                            [news_date_time, ticker, news_title, news_link, news_source])
+                        finviz_feed_data.append([news_date_time, ticker, news_title, news_link, news_source])
 
                         news_count += 1
 
-                print(f'{news_count} news found for {ticker}')
+                ticker_success_count += 1
+                print(f'{news_count} news found for {ticker}.')
 
         time.sleep(2)  # Pause between tickers
-        # Append data into csv file after accumulating the data for all tickers
-        writer.writerows(finviz_feed_data)
+        writer.writerows(finviz_feed_data)  # Append data into csv file after accumulating the data for all tickers
+    print(f'\nSuccessfully scrapped {ticker_success_count}/{len(tickers)} tickers')  # Indicate that the script is completed
 
 
 scraper(tickers)
